1b90c77792738b08f624c1ef2c5e77cb37b5a0d1
Marcelo Pico <mdpico3@utpl.edu.ec>
2026-05-14 07:36:30 -0500

    examen sin IA inicial

---
+++ pruebaparcial/src/test/java/ec/utpl/computacion/proava/parcial/AppTest.java
@@ -0,0 +1,20 @@
+package ec.utpl.computacion.proava.parcial;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
